41c8f2fd79658d90d8db301f2013e23c8bea5fd7
tim565 <schmitz.tim@protonmail.com>
2023-12-10 14:13:51 +0100

    Fixed division by zero error by checking for mission grades using if statement

--- 03-Debugging/upgrade_calculator.py
+++ 03-Debugging/upgrade_calculator.py
@@ -1,25 +1,7 @@
-# TODO 🏠: - Identify the cause of the exception by following these steps:
-#   1. Run the script.
-#   2. Set debugger breakpoints on any line numbers mentioned in the exception
-#      message you should receive.
-#   3. Run the debugger.
-#   4. Traverse through the breakpoints until you arrive at the line that the
-#       error occurs.
-#   4. Run the line of code causing the error in Debugger terminal to
-#      replicate and isolate the issue.
-#   5. Break down the problematic line into its components, such as variables,
-#      and execute them in the debugger terminal. This will isolate the error
-#      even further help identify why the error is occurring.
-#   Once you have isolated the error and understand the exact reason behind it,
-#   proceed with implementing a fix:
-#   - Wrap the problematic line within an `if` statement, addressing the
-#     specific scenario that triggers the error. Consider using `else` or
-#     `if not` as necessary to cover alternate cases.
-
 gradebook = {
-    "Alice": { "Math": 90, "Science": 95, "English": 85 },
-    "Bob": { "Math": 80, "Science": 88, "English": 89 },
-    "David": { },
+    "Alice": {"Math": 90, "Science": 95, "English": 85},
+    "Bob": {"Math": 80, "Science": 88, "English": 89},
+    "David": {},
 }
 
 
@@ -38,7 +20,11 @@ def calculate_average(student_name: str) -> int:
     student_exists = student_name in gradebook
     if student_exists:
         student_grades = gradebook[student_name].values()
-        average = sum(student_grades) / len(student_grades)
+
+        if student_grades:  # Check if there are grades to avoid division by zero
+            average = sum(student_grades) / len(student_grades)
+        else:
+            print(f"Student {student_name} has no grades.")
 
     if not student_exists:
         print(f'Student {student_name} does not exist.')
