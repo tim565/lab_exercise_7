916b5ab17a5cf955fc0e747569b59a049e43ad31
tim565 <schmitz.tim@protonmail.com>
2023-12-10 15:20:12 +0100

    Created tests for walker.py covering initialization, turn_right, turn_left, and moves

--- 07-Testing/turtlewalk/test_walker.py
+++ 07-Testing/turtlewalk/test_walker.py
@@ -1,5 +1,48 @@
 from .walker import Turtle, Direction  # Do not delete
 
-import pytest  # Do not delete
+# import pytest  # Do not delete
 
-# Your test code here
+import unittest
+
+
+class TestTurtle(unittest.TestCase):
+
+    def setUp(self):
+        self.turtle = Turtle(0, 0)
+
+    def test_initial_position_and_direction(self):
+        self.assertEqual(self.turtle.x, 0)
+        self.assertEqual(self.turtle.y, 0)
+        self.assertEqual(self.turtle.direction, Direction.NORTH)
+
+    def test_turn_right(self):
+        self.turtle.turn_right()
+        self.assertEqual(self.turtle.direction, Direction.EAST)
+
+    def test_turn_left(self):
+        self.turtle.turn_left()
+        self.assertEqual(self.turtle.direction, Direction.WEST)
+
+    def test_move_forward_north(self):
+        self.turtle.move_forward()
+        self.assertEqual(self.turtle.y, 1)
+
+    def test_move_forward_east(self):
+        self.turtle.turn_right()
+        self.turtle.move_forward()
+        self.assertEqual(self.turtle.x, 1)
+
+    def test_move_forward_south(self):
+        self.turtle.turn_right()
+        self.turtle.turn_right()
+        self.turtle.move_forward()
+        self.assertEqual(self.turtle.y, -1)
+
+    def test_move_forward_west(self):
+        self.turtle.turn_left()
+        self.turtle.move_forward()
+        self.assertEqual(self.turtle.x, -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
